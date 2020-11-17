1b81e1233063ba329fc615149f8aebdca322e67c
drfdev <drafff@gmail.com>
2020-11-17 15:31:35 +0300

    Notice: add first secondary element type

--- magic-box-elements/src/main/kotlin/dev/drf/magic/box/elements/SecondaryElementType.kt
+++ magic-box-elements/src/main/kotlin/dev/drf/magic/box/elements/SecondaryElementType.kt
@@ -1,5 +1,9 @@
 package dev.drf.magic.box.elements
 
-enum class SecondaryElementType {
-    // TODO
+enum class SecondaryElementType(
+        private val first: ElementType,
+        private val second: ElementType) {
+
+    DRAGON_FIRE(ElementType.FIRE, ElementType.FIRE)
+    // TODO add other combination + test
 }
