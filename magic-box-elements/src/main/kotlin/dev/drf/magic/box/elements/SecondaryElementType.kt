7891bcf0019f3985f4dad6e77e213d0db6630b38
drfdev <drafff@gmail.com>
2020-11-19 12:58:36 +0300

    Refactor: secondary elements

--- magic-box-elements/src/main/kotlin/dev/drf/magic/box/elements/SecondaryElementType.kt
+++ magic-box-elements/src/main/kotlin/dev/drf/magic/box/elements/SecondaryElementType.kt
@@ -4,6 +4,24 @@ enum class SecondaryElementType(
         private val first: ElementType,
         private val second: ElementType) {
 
-    DRAGON_FIRE(ElementType.FIRE, ElementType.FIRE)
+    /* Fire block */
+    DRAGON_FIRE(ElementType.FIRE, ElementType.FIRE),
+
+    /* Water block */
+    PURE_WATER(ElementType.WATER, ElementType.WATER),
+
+    /* Wind block */
+    HURRICANE(ElementType.WIND, ElementType.WIND)
+
+    /* Earth block */
+
+    /* Electric block */
+
+    /* Dark block */
+
+    /* Holy block */
+
+    /* Will block */
+
     // TODO add other combination + test
 }
