802ef5886e08805ccc038ea1720e05726fe43a76
drfdev <drafff@gmail.com>
2020-11-21 15:55:38 +0300

    Feature: add more secondary elements

--- magic-box-elements/src/main/kotlin/dev/drf/magic/box/elements/SecondaryElementType.kt
+++ magic-box-elements/src/main/kotlin/dev/drf/magic/box/elements/SecondaryElementType.kt
@@ -6,9 +6,21 @@ enum class SecondaryElementType(
 
     /* Fire block */
     DRAGON_FIRE(ElementType.FIRE, ElementType.FIRE),
+    LIGHTNING(ElementType.FIRE, ElementType.ELECTRIC),
+    FIRE_STORM(ElementType.FIRE, ElementType.WIND),
+    MAGMA(ElementType.FIRE, ElementType.EARTH),
+    DARK_FIRE(ElementType.FIRE, ElementType.DARK),
+    HOLY_LIGHT(ElementType.FIRE, ElementType.HOLY),
+    INNER_FIRE(ElementType.FIRE, ElementType.WILL),
 
     /* Water block */
     PURE_WATER(ElementType.WATER, ElementType.WATER),
+    ICE(ElementType.WATER, ElementType.WIND),
+    NATURE(ElementType.WATER, ElementType.EARTH),
+    ELECTRIC_STREAM(ElementType.WATER, ElementType.ELECTRIC),
+    DARK_WATER(ElementType.WATER, ElementType.DARK),
+    HOLY_WATER(ElementType.WATER, ElementType.HOLY),
+    VITALITY(ElementType.WATER, ElementType.WILL),
 
     /* Wind block */
     HURRICANE(ElementType.WIND, ElementType.WIND)
